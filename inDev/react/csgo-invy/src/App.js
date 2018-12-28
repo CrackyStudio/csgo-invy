511157540ebcf41b5b02ee1f2e9091e7f0d3ef6c
Antoine Nivoy <antoine.nivoy@gmail.com>
2018-12-28 16:31:01 +0100

    commit

--- inDev/react/csgo-invy/src/App.js
+++ inDev/react/csgo-invy/src/App.js
@@ -9,7 +9,7 @@ import Component from "@reactions/component";
 let missingWeaponSkinsArray = [];
 let weaponList = ["AK-47", "AUG", "AWP", "Bayonet", "Bowie Knife", "Butterfly Knife", "CZ75-Auto", "Desert Eagle", "Dual Barettas", 
 "Falchion Knife", "FAMAS", "Five-SeveN", "Flip Knife", "G3SG1", "Galil AR", "Glock-18", "Gut Knife", "Huntsman Knife", "Karambit", 
-"M249", "M4A1-S", "M4A4", "M9 Bayonet", "MAC-10", "MAG-7", "MP7", "MP9", "Negev", "Nova", "P2000", "P250", "P90", "PP-Bizon", 
+"M249", "M4A1-S", "M4A4", "M9 Bayonet", "MAC-10", "MAG-7", "MP5-SD", "MP7", "MP9", "Negev", "Nova", "P2000", "P250", "P90", "PP-Bizon", 
 "R8 Revolver", "Sawed-Off", "SCAR-20", "SG 553", "Shadow Daggers", "SSG 08", "Tec-9", "UMP-45", "USP-S", "XM1014"];
 
 class App extends Component {
