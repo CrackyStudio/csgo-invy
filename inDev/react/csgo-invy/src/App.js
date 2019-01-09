ead4a960d8121ecc2ce04c2166ef06bf0db55a3d
Antoine Nivoy <antoine.nivoy@gmail.com>
2019-01-09 13:30:00 +0100

    Update App.js

--- inDev/react/csgo-invy/src/App.js
+++ inDev/react/csgo-invy/src/App.js
@@ -126,7 +126,6 @@ class App extends Component {
           if (json.success === true) {
             if (json.listinginfo.length !== 0) {
               link = `${json.results_html.match(/" src=(.*)" srcset/)[1].substr(1).slice(0, -7)}360fx360f`
-              // DON T FORGET TO ADD NAVAJA KNIFE, STILETTO KNIFE, TALON KNIFE AND URSUS KNIFE IN WEAPON LIST
               // weapon marketname :
               // Bowie Knife = knife_survival_bowie
               // Butterfly Knife = knife_butterfly
